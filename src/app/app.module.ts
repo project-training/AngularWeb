3a037914fce418a0a54d971b262d5b96fee2f407
angpao <angpao.kittimasak@gmail.com>
2019-04-13 18:34:16 +0700

    generate user component and sign-up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { SignUpComponent } from './components/user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
