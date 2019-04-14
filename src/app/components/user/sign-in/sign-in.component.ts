f430f0ff451b6130731bb9e25ef4eb3a29e6fcda
angpao <angpao.kittimasak@gmail.com>
2019-04-14 19:35:25 +0700

    Design SignIn page

--- src/app/components/user/sign-in/sign-in.component.ts
+++ src/app/components/user/sign-in/sign-in.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
+  model = {
+    email: '',
+    password: ''
+  }
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   constructor() { }
 
   ngOnInit() {
