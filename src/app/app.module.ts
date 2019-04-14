1de445d7973e465d2190c99a558e106e4fb487c6
angpao <angpao.kittimasak@gmail.com>
2019-04-14 19:10:35 +0700

    generate sign-in component generate user-profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { SignUpComponent } from './components/user/sign-up/sign-up.component';
+import { SignInComponent } from './components/user/sign-in/sign-in.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    SignUpComponent
+    SignUpComponent,
+    SignInComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
